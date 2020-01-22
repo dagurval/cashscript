f9dde2de6448ae543095e2b229d54d22a2ff30d1
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2020-01-22 08:14:27 +0000

    add checksig

--- examples/bug.ts
+++ examples/bug.ts
@@ -10,6 +10,14 @@ export async function run(): Promise<void> {
   const network = 'mainnet';
   const bitbox = new BITBOX({ restURL: 'https://rest.bitcoin.com/v2/' });
 
+  const rootSeed = bitbox.Mnemonic.toSeed('CashScript');
+  const hdNode = bitbox.HDNode.fromSeed(rootSeed, network);
+
+  const alice = bitbox.HDNode.toKeyPair(bitbox.HDNode.derive(hdNode, 0));
+  const alicePk = bitbox.ECPair.toPublicKey(alice);
+  const alicePkh = bitbox.Crypto.hash160(alicePk);
+
+
   const eatbch = "bitcoincash:pp8skudq3x5hzw8ew7vzsw8tn4k8wxsqsv0lt0mf3g";
 
   const Bug = Contract.compile(path.join(__dirname, 'bug.cash'), network);
@@ -23,8 +31,8 @@ export async function run(): Promise<void> {
   console.log('contract opcount:', instance.opcount);
   console.log('contract bytesize:', instance.bytesize);
 
-  const tx = await instance.functions.yum()
-    .send(eatbch, 600, { })
+  const tx = await instance.functions.yum(alicePk, new Sig(alice))
+    .send(eatbch, 546, { fee: 523 })
     .catch((e) => { console.log(e) })
 
   console.log('transaction details:', tx);
